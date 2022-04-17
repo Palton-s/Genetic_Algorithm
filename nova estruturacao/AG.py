03aa6d68dbc1756d01878006e23781fdd7249ffa
Palton-s <55421324+Palton-s@users.noreply.github.com>
2022-04-16 21:47:40 -0300

    Update AG.py

--- nova estruturacao/AG.py
+++ nova estruturacao/AG.py
@@ -5,14 +5,14 @@ import avaliacao as av
 import funcoes_auxiliares as aux
 
 # define informações da população
-NV = 3
-N = 40
-n_bits = 1024
-percent_elite = 0.25
-n_cortes_no_cruzamento = 8
-n_geracoes = 100
-taxa_de_mutacao = 0.1
-intensidade_da_mutacao = 0.1
+NV = 5 # número de variáveis
+N = 40 # numero de individuos
+n_bits = 1024 # número de bits por variável em cada indivíduo
+percent_elite = 0.25 # percentual dos melhores indivíduos que serão conservados para a próxima geração
+n_cortes_no_cruzamento = 2 # número de pontos de corte no cruzamento
+n_geracoes = 100 # número de gerações
+taxa_de_mutacao = 0.1 # taxa de mutação
+intensidade_da_mutacao = 0.3 # intensidade da mutação
 
 # inicia a população
 X = populacao.cria_populacao(NV, N, n_bits)
